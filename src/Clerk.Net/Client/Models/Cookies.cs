3acf4e14fcfaf6408d1686cef375540fc1c6f0f4
JT <Hawxy@users.noreply.github.com>
2025-04-21 14:25:17 +0800

    v1.14.0

--- src/Clerk.Net/Client/Models/Cookies.cs
+++ src/Clerk.Net/Client/Models/Cookies.cs
@@ -9,27 +9,28 @@ namespace Clerk.Net.Client.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class User_external_accounts : IAdditionalDataHolder, IParsable
+    public partial class Cookies : IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>
-        /// Instantiates a new <see cref="global::Clerk.Net.Client.Models.User_external_accounts"/> and sets the default values.
-        /// </summary>
-        public User_external_accounts()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
+        /// <summary>Array of cookie directives.</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public List<string>? CookiesProp { get; set; }
+#nullable restore
+#else
+        public List<string> CookiesProp { get; set; }
+#endif
+        /// <summary>String representing the object&apos;s type. Objects of the same type share the same value.</summary>
+        public global::Clerk.Net.Client.Models.Cookies_object? Object { get; set; }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Clerk.Net.Client.Models.User_external_accounts"/></returns>
+        /// <returns>A <see cref="global::Clerk.Net.Client.Models.Cookies"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Clerk.Net.Client.Models.User_external_accounts CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Clerk.Net.Client.Models.Cookies CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Clerk.Net.Client.Models.User_external_accounts();
+            return new global::Clerk.Net.Client.Models.Cookies();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -39,6 +40,8 @@ namespace Clerk.Net.Client.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "cookies", n => { CookiesProp = n.GetCollectionOfPrimitiveValues<string>()?.AsList(); } },
+                { "object", n => { Object = n.GetEnumValue<global::Clerk.Net.Client.Models.Cookies_object>(); } },
             };
         }
         /// <summary>
@@ -48,7 +51,8 @@ namespace Clerk.Net.Client.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteAdditionalData(AdditionalData);
+            writer.WriteCollectionOfPrimitiveValues<string>("cookies", CookiesProp);
+            writer.WriteEnumValue<global::Clerk.Net.Client.Models.Cookies_object>("object", Object);
         }
     }
 }
